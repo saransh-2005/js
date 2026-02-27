89382592e85dab9ae0ba609b8ca0eb4908ea3873
saransh-2005 <saranshkotuli@gmail.com>
2026-02-27 12:15:25 +0000

    container updated

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -1,11 +1,11 @@
 const accountId  = 2300165 //we can't change it
 let accountEmail = "saransh@google.com"
-var accountPass = "kotuli*123" //we should not use var
+var accountPass = "saru*123" //we should not use var
 accountCity = "kanpur"
 let accountState;
 console.log(accountId)
 
 accountEmail = "saransh17092005@gmail.com"
-accountPass = "kotuli*17"
+accountPass = "saru*17"
 accountCity = "Ghaziabad"
 console.table([accountId,accountEmail,accountPass,accountCity,accountState])
