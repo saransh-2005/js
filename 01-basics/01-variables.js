f701bb5aff4c105cd073f78fc30c3492f29c7fd2
saransh-2005 <saranshkotuli@gmail.com>
2026-02-27 18:40:59 +0530

    Improve comments on variable declarations
    Updated comments to clarify variable usage and scope issues.

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -1,6 +1,6 @@
 const accountId  = 2300165 //we can't change it
 let accountEmail = "saransh@google.com"
-var accountPass = "saru*123" //we should not use var
+var accountPass = "saru*123" //we should not use var because of issue in block scope and functional scope
 accountCity = "kanpur"
 let accountState;
 console.log(accountId)
